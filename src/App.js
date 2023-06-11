8e42d9c1303c47128d0c35d554fd0173a428f181
KrishnaDass95 <krishdass95@gmail.com>
2023-06-11 12:39:21 +0530

    Basic nav links added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
-
+import { Routes, Route } from "react-router-dom";
 const App = () => {
 
   return(
     <div>
-      <Header />
-      Hello
+      <Routes>
+        <Route path="/" element={<Header />}></Route>  
+      </Routes>
     </div>
   )
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,18 @@
 import "./style.css"
-
+import { Link } from "react-router-dom";
 
 const Header = () => {
 
     return(
         <>
-        <div className="nav">HEADER</div>
+        <div className="navbar">
+            <div className="links">
+                <Link to="/">Signup</Link>
+                <Link to="/podcasts">Podcasts</Link>
+                <Link to="/start-a-podcast">Start A Podcast</Link>
+                <Link to="/profile">Profile</Link>
+            </div>
+        </div>
         </>
     )
 

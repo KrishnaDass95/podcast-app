8864c3736c372d0ee1ec1ca646d39a424262b74a
KrishnaDass95 <krishdass95@gmail.com>
2023-06-11 13:06:48 +0530

    Finish Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,29 @@
 import "./style.css"
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+
+    const location = useLocation();
+    const currentPath = location.pathname;
 
     return(
         <>
         <div className="navbar">
             <div className="gradient"></div>
             <div className="links">
-                <Link to="/">Signup</Link>
-                <Link to="/podcasts">Podcasts</Link>
-                <Link to="/start-a-podcast">Start A Podcast</Link>
-                <Link to="/profile">Profile</Link>
+                <Link to="/" 
+                className={currentPath == '/' ? 'active' : ''}
+                 >Signup</Link>
+                <Link to="/podcasts"
+                className={currentPath == '/podcasts' ? 'active' : ''}
+                >Podcasts</Link>
+                <Link to="/start-a-podcast"
+                className={currentPath == '/start-a-podcast' ? 'active' : ''}
+                >Start A Podcast</Link>
+                <Link to="/profile"
+                className={currentPath == '/profile' ? 'active' : ''}
+                >Profile</Link>
             </div>
         </div>
         </>

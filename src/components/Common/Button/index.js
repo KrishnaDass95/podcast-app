4d933d01efb8fe510126b7d535d342c58e22364a
KrishnaDass95 <krishdass95@gmail.com>
2023-06-14 19:45:17 +0530

    Finish styling of buttons

--- src/components/Common/Button/index.js
+++ src/components/Common/Button/index.js
@@ -1,9 +1,7 @@
+import "./style.css";
 
-
-const Button = ({text, onClick}) => {
-
-    return(
-        <div onClick={onClick}>{text}</div>
-    )
-}
+const Button = ({ text, onClick }) => {
+  return <div className="custom-btn"
+  onClick={onClick}>{text}</div>;
+};
 export default Button;

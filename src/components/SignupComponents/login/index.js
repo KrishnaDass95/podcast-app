80b9b8e06611529152cbdd96e7575096536617d9
KrishnaDass95 <krishdass95@gmail.com>
2023-06-15 11:05:45 +0530

    Finish toastify and error handling on Login page

--- src/components/SignupComponents/login/index.js
+++ src/components/SignupComponents/login/index.js
@@ -7,42 +7,55 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { doc, getDoc } from "firebase/firestore";
 import { setUser } from "../../../slices/userSlice";
-
+import { toast } from "react-toastify";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleLogin = async () => {
+    setLoading(true);
     console.log("login ....");
-    try {
-      // firebase sign in
-      const userCredentials = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      const user = userCredentials.user;
+    if (email && password) {
+      try {
+        // firebase sign in
+        const userCredentials = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        const user = userCredentials.user;
 
-      const userDoc = await getDoc(doc(db, "users", user.uid));
-      const userData = userDoc.data();
-      console.log(
-        "user data returned from firebase db after signing in ",
-        userData
-      );
+        const userDoc = await getDoc(doc(db, "users", user.uid));
+        const userData = userDoc.data();
+        console.log(
+          "user data returned from firebase db after signing in ",
+          userData
+        );
 
-      dispatch(setUser({
-        name: userData.name,
-        email: userData.email,
-        uid: userData.uid,
-      }));
+        dispatch(
+          setUser({
+            name: userData.name,
+            email: userData.email,
+            uid: userData.uid,
+          })
+        );
 
-      navigate("/profile");
-
-    } catch (e) {
-      console.log("Error", e);
+        setLoading(false);
+        navigate("/profile");
+      } catch (e) {
+        console.log("Error", e);
+        toast.error(e.message);
+        setLoading(false);
+      }
+    }
+    else{
+        if(!email || !password){
+            toast.error('Fields empty, please enter email and password');
+        }
     }
   };
 
@@ -64,7 +77,7 @@ const LoginForm = () => {
         required={true}
       />
 
-      <Button text="login" onClick={handleLogin} />
+      <Button text={loading ? 'loading' : 'login'} onClick={handleLogin} disabled={loading}/>
     </div>
   );
 };

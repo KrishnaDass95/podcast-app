d756f8dc049d927f9d4a4c9882c91783d06ba2fa
KrishnaDass95 <krishdass95@gmail.com>
2023-06-15 09:36:55 +0530

    Finish firebase signup and login

--- src/components/SignupComponents/login/index.js
+++ src/components/SignupComponents/login/index.js
@@ -1,42 +1,72 @@
 import Input from "../../Common/Input";
 import { useState } from "react";
 import Button from "../../Common/Button";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../../../firebase";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { setUser } from "../../../slices/userSlice";
+
 
 const LoginForm = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const handleLogin = () => {
-        console.log("login");
+  const handleLogin = async () => {
+    console.log("login ....");
+    try {
+      // firebase sign in
+      const userCredentials = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredentials.user;
+
+      const userDoc = await getDoc(doc(db, "users", user.uid));
+      const userData = userDoc.data();
+      console.log(
+        "user data returned from firebase db after signing in ",
+        userData
+      );
+
+      dispatch(setUser({
+        name: userData.name,
+        email: userData.email,
+        uid: userData.uid,
+      }));
+
+      navigate("/profile");
+
+    } catch (e) {
+      console.log("Error", e);
     }
+  };
 
-    return(
-        <div className="input-wrapper">
-            <Input 
-            type="text"
-            placeholder="Email"
-            value={email}
-            setState={setEmail}
-            required={true}
-            />
+  return (
+    <div className="input-wrapper">
+      <Input
+        type="text"
+        placeholder="Email"
+        value={email}
+        setState={setEmail}
+        required={true}
+      />
 
-            <Input 
-            type="password"
-            placeholder="Password"
-            value={password}
-            setState={setPassword}
-            required={true}
-            />
+      <Input
+        type="password"
+        placeholder="Password"
+        value={password}
+        setState={setPassword}
+        required={true}
+      />
 
-            <Button 
-            text="login"
-            onClick={handleLogin}
-            />
-
-            
-
-        </div>
-    )
-}
+      <Button text="login" onClick={handleLogin} />
+    </div>
+  );
+};
 
 export default LoginForm;
--- src/pages/profilePage.js
+++ src/pages/profilePage.js
@@ -1,9 +1,14 @@
+import { useSelector } from "react-redux";
 import Header from "../components/Header"
 
 const ProfilePage = () => {
+    const user = useSelector((state) => state.user.user);
     return(
         <div>
             <Header />
+            <h1>{user.name}</h1>
+            <h1>{user.email}</h1>
+            <h1>{user.uid}</h1>
         </div>
     )
 }

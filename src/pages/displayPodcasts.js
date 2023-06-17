1c167086e36adf0a862bb4976600a2fa6871841f
KrishnaDass95 <krishdass95@gmail.com>
2023-06-17 17:13:45 +0530

    test is podcasts are stored on the app

--- src/pages/displayPodcasts.js
+++ src/pages/displayPodcasts.js
@@ -1,46 +1,49 @@
 import { useDispatch, useSelector } from "react-redux";
-import Header from "../components/Header"
+import Header from "../components/Header";
 import { useEffect } from "react";
 import { collection, onSnapshot, query } from "firebase/firestore";
 import { db } from "../firebase";
 import { setPodcasts } from "../slices/podcastSlice";
 
 const PodcastsPage = () => {
+  const dispatch = useDispatch();
+  const podcasts = useSelector((state) => state.podcasts.podcasts);
 
-    const dispatch = useDispatch();
-    const podcasts = useSelector((state) => state.podcasts.podcasts)
+  // We need to get the podcasts from db and store it in our redux state
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "podcasts")),
+      (querySnapshot) => {
+        const podcastsData = [];
+        querySnapshot.forEach((doc) => {
+          podcastsData.push({ id: doc.id, ...doc.data() });
+        });
+        dispatch(setPodcasts(podcastsData));
+      },
+      (error) => {
+        console.error("Error fetching podcasts", error);
+      }
+    );
 
-    // We need to get the podcasts from db and store it in our redux state
-    useEffect(() => {
-        const unsubscribe = onSnapshot(
-            query(collection(db, "podcasts")),
-            (querySnapshot) => {
-                const podcastsData = [];
-                querySnapshot.forEach((doc) => {
-                    podcastsData.push({id: doc.id, ...doc.data()})
-                });
-                dispatch(setPodcasts(podcastsData))
-            },
-            (error) => {
-                console.error("Error fetching podcasts", error);
-            }
-        );
-
-        return () => {
-            unsubscribe();
-        }
-
-    }, [dispatch])
-    return(
-        <div>
-            <Header />
-            <h1>Discover Podcasts</h1>
-            <div class="input-wrapper">
-                {podcasts.length > 0 ?  <>Podcasts present</> : <>Podcasts absent</>}
-
-            </div>
-        </div>
-    )
-}
+    return () => {
+      unsubscribe();
+    };
+  }, [dispatch]);
+  return (
+    <div>
+      <Header />
+      <h1>Discover Podcasts</h1>
+      <div class="input-wrapper">
+        {podcasts.length > 0 ? (
+            <>
+            {podcasts.map((item) => {
+                return <p>{item.title}</p>
+            })}
+            </>
+        ) : <>Podcasts absent</>}
+      </div>
+    </div>
+  );
+};
 
 export default PodcastsPage;

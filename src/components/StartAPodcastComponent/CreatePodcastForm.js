5418b2a31db76d7ef8ad18f25f0f60c7bf11c7eb
KrishnaDass95 <krishdass95@gmail.com>
2023-06-17 09:55:36 +0530

    Take podcast data from form and add it to storage and db of firebase

--- src/components/StartAPodcastComponent/CreatePodcastForm.js
+++ src/components/StartAPodcastComponent/CreatePodcastForm.js
@@ -2,6 +2,10 @@ import Input from "../Common/Input";
 import { useState } from "react";
 import FileInput from "../Common/Input/FileInput";
 import Button from "../Common/Button";
+import { toast } from "react-toastify";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage"
+import { db, auth, storage } from "../../firebase";
+import { addDoc, collection } from "firebase/firestore";
 
 
 const CreatePodcastForm = () => {
@@ -9,7 +13,47 @@ const CreatePodcastForm = () => {
     const [title, setTitle] = useState("");
     const [desc, setDesc] = useState("");
     const [displayImage, setDisplayImage] = useState();
-    const [bannerImage, setBannerImage] = useState(); 
+    const [bannerImage, setBannerImage] = useState();
+    const [loading, setLoading] = useState(false); 
+
+
+    const handleSubmit = async () => {
+        if(title && desc && displayImage && bannerImage){
+
+            //1. Upload the files to firebase storage to get downloadable links
+            const bannerImageRef = ref(storage, `podcasts/${auth.currentUser.uid}/${Date.now()}`);
+            await uploadBytes(bannerImageRef, bannerImage);
+
+            const displayImageRef = ref(storage, `podcasts/${auth.currentUser.uid}/${Date.now()}` );
+            await uploadBytes(displayImageRef, displayImage);
+
+            // get downloadable link
+            const bannerImageUrl = await getDownloadURL(bannerImageRef);
+            const displayImageUrl = await getDownloadURL(displayImageRef);
+
+            // 2. create a new doc in a new collection called podcasts
+            const podcastData = {
+                title,
+                description: desc,
+                bannerImage: bannerImageUrl,
+                displayImage: displayImageUrl,
+                createdBy: auth.currentUser.uid,
+            }
+
+            const docRef = await addDoc(collection(db, "podcasts"), podcastData);
+            setTitle("");
+            setDesc("");
+            setBannerImage(null);
+            setDisplayImage(null);
+            
+            toast.success('Podcast created');
+
+            // 3. Save this new podcast episode state in podcasts
+        }
+        else{
+            toast.error("All fields are important, make sure to fill them all");
+        }
+    }
 
     const bannerImageFileHandleFunc = (file) => {
         setBannerImage(file)
@@ -53,7 +97,7 @@ const CreatePodcastForm = () => {
 
              
 
-            <Button text="Create podcast"/>
+            <Button text="Create podcast" onClick={handleSubmit}/>
 
             
 

d470b44018b15e662ac4b4c998fd4c513591beba
KrishnaDass95 <krishdass95@gmail.com>
2023-06-17 17:49:35 +0530

    Add a search bar and also add searching logic on podcasts page

--- src/pages/displayPodcasts.js
+++ src/pages/displayPodcasts.js
@@ -1,14 +1,16 @@
 import { useDispatch, useSelector } from "react-redux";
 import Header from "../components/Header";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { collection, onSnapshot, query } from "firebase/firestore";
 import { db } from "../firebase";
 import { setPodcasts } from "../slices/podcastSlice";
 import PodcastCard from "../components/PodcastComponents/PodcastCard";
+import Input from "../components/Common/Input";
 
 const PodcastsPage = () => {
   const dispatch = useDispatch();
   const podcasts = useSelector((state) => state.podcasts.podcasts);
+  const [searchTerm, setSearchTerm] = useState("");
 
   // We need to get the podcasts from db and store it in our redux state
   useEffect(() => {
@@ -30,16 +32,27 @@ const PodcastsPage = () => {
       unsubscribe();
     };
   }, [dispatch]);
+
+  let filteredPodcasts = podcasts.filter((item) => item.title.trim().toLowerCase().includes(searchTerm.toLowerCase()))
+
   return (
     <div>
       <Header />
       <h1>Discover Podcasts</h1>
       <div class="input-wrapper">
+      <Input
+        type="text"
+        value={searchTerm}
+        placeholder="Search Podcasts"
+        required={true}
+        setState={setSearchTerm}
+      />
+
         {
             podcasts.length > 0 ?
              (
                 <div className="podcasts-flex">
-                    {podcasts.map((item) => {
+                    {filteredPodcasts.map((item) => {
                         return <PodcastCard 
                         key={item.id}
                         id={item.id}
